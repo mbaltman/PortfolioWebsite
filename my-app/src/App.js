318651315a497f6a5e7f5b236e37fd4676455213
mbaltman <meggersa@gmail.com>
2024-11-03 15:43:45 -0500

    Remove all dead code and convert to new version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import pixelDesktop from './imgs/ComputerPixelArt-HighResolution.png';
+
+import './css/pixelDesktop.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="fullScreen" id="fullScreenImage">
+              <img id="pixelDesktopImg"
+                   src={pixelDesktop}
+                   alt="image of a desk, with two monitors, rendered in a pixel art style "/>
+          </div>
+
+          <div className="fullScreen" id="fullScreenContent">
+              <div id="navBar">
+
+              </div>
+
+              <div id="content">
+
+              </div>
+          </div>
+      </div>
   );
 }
 
