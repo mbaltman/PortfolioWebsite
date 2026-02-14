a7787f82768a7bab4dcfa50b5266e6d0a257f404
mbaltman <meggersa@gmail.com>
2026-02-14 02:22:56 -0500

    add basic tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from "next/script";
 
 export default function RootLayout({
                                        children,
@@ -7,7 +8,34 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>{children}
+
+        <Script id="statcounter" strategy="afterInteractive">
+            {`
+                        var sc_project=13204934;
+                        var sc_invisible=1;
+                        var sc_security="9c62e743";
+                    `}
+        </Script>
+        <Script
+            src="https://www.statcounter.com/counter/counter.js"
+            strategy="afterInteractive"
+            async
+        />
+        <noscript>
+            <div className="statcounter">
+                <a title="hit counter" href="https://statcounter.com/" target="_blank">
+                    <img
+                        className="statcounter"
+                        src="https://c.statcounter.com/13204934/0/9c62e743/1/"
+                        alt="hit counter"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
+                </a>
+            </div>
+        </noscript>
+        
+        </body>
         </html>
     )
 }
